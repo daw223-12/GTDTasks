56b881eee79bd7098f8ab6b9dc2423249a22ac2f
Francisco-Serbatic <francisco.juesas.franco@iesjulianmarias.es>
2024-05-09 16:43:22 +0200

    No sé la verdad. Algo en el front que se rompen cosas

--- GTDTasks-Front/src/app/pages/login-page/login-page.component.ts
+++ GTDTasks-Front/src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  mostrarHijos: boolean = true;
+  ruta!: string;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.url.subscribe(segments => {
+      this.ruta = segments.join('/');
+    });
+  }
 }
